a2d7d3b1b11af9581cfb7853d25a179ef0ae1ca3
fareya <fareya.ikram@gmail.com>
2019-11-25 13:26:12 -0500

    some changes

--- scripts/extraScripts/naml_original.py
+++ scripts/extraScripts/naml_original.py
@@ -25,24 +25,22 @@ import json
 from datetime import date  
 from datetime import datetime
 #setting constants 
-
 classifier={
     'TSF_CLF':0,
     'KNN_CLF':1,
     'PF_CLF':2,
 }
-TO_LOG= True
+TO_LOG= False 
 
-TSF_default_parameters={'base_estimator':None, 'n_estimators':10, 'criterion':'entropy', 'max_depth':None, 'min_samples_split':2, 'min_samples_leaf':1, 'min_weight_fraction_leaf':0.0, 'max_features':None, 'max_leaf_nodes':None, 'min_impurity_decrease':0.0, 'min_impurity_split':None, 'bootstrap':False, 'oob_score':False, 'n_jobs':None, 'random_state':None, 'verbose':0, 'warm_start':False, 'class_weight':None}
+#concatenates ....
+#make this assumption cleare 
+#write better columns 
+#error messages
+#define constants when refactoring(readability)
 
-def list_to_dict(a): 
-    it = iter(a) 
-    res_dct = dict(zip(it, it)) 
-    return res_dct 
+#extracting the data from the csv as a dataframe and reformatting it 
 
-#extracting the data from the csv as a dataframe and reformatting it for sktime compatibility 
 def reformatData(target, file_name):
-    
     print("reformatting the data...")
     raw_df= pd.read_csv(file_name)
     
@@ -53,30 +51,24 @@ def reformatData(target, file_name):
 
     sorted_long_table_df=long_table_df.sort_values(by=['event','name','start time','channel'], axis=0)
 
-    #start time qualifies as a unique start time
-    #maybe will switch this out later 
     unique_dim_ids = sorted_long_table_df.iloc[:, 4].unique()
 
-    #replacing channel named to numeric values (need to do this doem the from_long_to_nested function)
     for i in range(len(unique_dim_ids)):
         my_channel=unique_dim_ids[i]
         sorted_long_table_df['channel']=sorted_long_table_df['channel'].replace({my_channel:i})
     unique_start_time = sorted_long_table_df.iloc[:, 2].unique()
 
-    #replacing start time column to numeric values (need to do this doem the from_long_to_nested function)
     for i in range(len(unique_start_time)):
         my_time=unique_start_time[i]
         sorted_long_table_df['start time']=sorted_long_table_df['start time'].replace({my_time:i})
 
-    #excess columns are dropped for the frome_long_to_nested function
+    
     sorted_long_table_df_stripped=sorted_long_table_df.drop(columns=['event','name','end time'])
 
-    #sorted_long_table_df_stripped.head()
-    
-    #table goes from long to nested 
+    sorted_long_table_df_stripped.head()
     df_nested = from_long_to_nested(sorted_long_table_df_stripped)
 
-    #create a list of labels 
+
     new_unique_start_time=sorted_long_table_df.iloc[:, 2].unique()
     labels=[]
     for e in new_unique_start_time:
@@ -86,9 +78,53 @@ def reformatData(target, file_name):
     np_labels= np.asarray(labels, dtype=np.str)
     
     return df_nested, np_labels 
+# #extracting the data from the csv as a dataframe and reformatting it for sktime compatibility 
+# def reformatData(target, file_name):
+    
+#     print("reformatting the data...")
+#     raw_df= pd.read_csv(file_name)
+    
+#     #collapses the time cols into one single time column to match the rest of the columns 
+#     long_table_df= raw_df.melt(id_vars=["event", "name","start time", "end time","channel"], 
+#             var_name="anindex", 
+#             value_name="value")
 
-# splitting the test train based on the test train that is specified 
-# i think that there might be issues with this function 
+#     sorted_long_table_df=long_table_df.sort_values(by=['event','name','start time','channel'], axis=0)
+
+#     #start time qualifies as a unique start time
+#     #maybe will switch this out later 
+#     unique_dim_ids = sorted_long_table_df.iloc[:, 4].unique()
+
+#     #replacing channel named to numeric values (need to do this doem the from_long_to_nested function)
+#     for i in range(len(unique_dim_ids)):
+#         my_channel=unique_dim_ids[i]
+#         sorted_long_table_df['channel']=sorted_long_table_df['channel'].replace({my_channel:i})
+#     unique_start_time = sorted_long_table_df.iloc[:, 2].unique()
+
+#     #replacing start time column to numeric values (need to do this doem the from_long_to_nested function)
+#     for i in range(len(unique_start_time)):
+#         my_time=unique_start_time[i]
+#         sorted_long_table_df['start time']=sorted_long_table_df['start time'].replace({my_time:i})
+
+#     #excess columns are dropped for the frome_long_to_nested function
+#     sorted_long_table_df_stripped=sorted_long_table_df.drop(columns=['event','name','end time'])
+
+#     #sorted_long_table_df_stripped.head()
+    
+#     #table goes from long to nested 
+#     df_nested = from_long_to_nested(sorted_long_table_df_stripped)
+
+#     #create a list of labels 
+#     new_unique_start_time=sorted_long_table_df.iloc[:, 2].unique()
+#     labels=[]
+#     for e in new_unique_start_time:
+#         x=sorted_long_table_df.loc[sorted_long_table_df['start time']==e,[target]].iloc[0][0]
+#         labels.append(x)
+
+#     np_labels= np.asarray(labels, dtype=np.str)
+    
+#     return df_nested, np_labels 
+
 def splitTestTrain(X, y, percent_train):
     msk = np.random.rand(len(X)) < percent_train
     ytrain=y[msk]
@@ -98,21 +134,15 @@ def splitTestTrain(X, y, percent_train):
     
     return Xtrain, ytrain, Xtest, ytest
 
-
-#concatenate multivariate time series and classify using the specified classifier 
-def concatenateMethod(classifier_num, X, y, percent_train):#,clf_parameters):
+def concatenateMethod(classifier_num, X, y, percent_train):
     print("classification...")
     concatenation_instance=ColumnConcatenator()
     X_concatenated=concatenation_instance.fit(X).transform(X)
     Xtrain, ytrain, Xtest, ytest= splitTestTrain(X_concatenated,y,percent_train)
+    print("Xtrain shape")
+    print(Xtrain.shape)
+    #print(Xtrain.head())
     if(classifier_num==classifier['TSF_CLF']):
-        #TSF_params=TSF_default_parameters
-        #clf_params_dict=list_to_dict(clf_parameters)
-        #print(TSF_params)
-        #for e in clf_params_dict:
-         #   TSF_params[e]=clf_params_dict[e]
-        #print(TSF_params)
-       # clf= TimeSeriesForestClassifier(base_estimator=TSF_params['base_estimator'], n_estimators=TSF_params['n_estimators'], criterion=TSF_params['criterion'], max_depth=TSF_params['max_depth'], min_samples_split=TSF_params['min_samples_split'], min_samples_leaf=TSF_params['min_samples_leaf'], min_weight_fraction_leaf=TSF_params['min_weight_fraction_leaf'], max_features=TSF_params['max_features'], max_leaf_nodes=TSF_params['max_leaf_nodes'], min_impurity_decrease=TSF_params['min_impurity_decrease'], min_impurity_split=TSF_params['min_impurity_split'], bootstrap=TSF_params['bootstrap'], oob_score=TSF_params['oob_score'], n_jobs=TSF_params['n_jobs'], random_state=TSF_params['random_state'], verbose=TSF_params['verbose'], warm_start=TSF_params['warm_start'], class_weight=TSF_params['class_weight'])
         clf= TimeSeriesForestClassifier(n_estimators=10)
         clf.fit(Xtrain, ytrain)
         return clf.score(Xtest,ytest)
@@ -125,9 +155,8 @@ def concatenateMethod(classifier_num, X, y, percent_train):#,clf_parameters):
         pf.fit(Xtrain, ytrain)
         return pf.score(Xtest, ytest)
     else: 
-        raise ValueError("Specified classifier is not an option")
-
-
+        return 0
+        
 #multivariate shapelet method         
 def multivariateShapeletMethod(X, y, percent_train, time_contract=0.5):
     Xtrain, ytrain, Xtest, ytest= splitTestTrain(X,y,percent_train)
@@ -135,18 +164,18 @@ def multivariateShapeletMethod(X, y, percent_train, time_contract=0.5):
     clf.fit(Xtrain, ytrain)
     return clf.score(Xtest, ytest)
 
-
 def main():
     script = sys.argv[0]
     json_file_name = sys.argv[1]
-
-    #reading the inputted json file 
-    print(json_file_name)
-    
+    file_write_name=str(date.today())+":"+str(datetime.time(datetime.now()))+'.txt'
+    file_write= open(file_write_name,"w+")
+    #write to file here 
+    file_write.write(json_file_name)
+    print(json_file_name) 
     with open(json_file_name) as f:
         data = json.load(f)
-
     print(json.dumps(data, indent=4, sort_keys=True))    
+  
 
     #setting values given the configuration files 
     target=data['targetCol']
@@ -173,7 +202,7 @@ def main():
         print(job)
         start_time=time.time()
         if(job['method']=='UNIVARIATE_TRANSFORMATION'):
-            acc= concatenateMethod(classifier[job['classifier']], X, y, percent_train)#,job['parameters'])
+            acc= concatenateMethod(classifier[job['classifier']], X, y, percent_train)
         elif(job['method']=='SHAPELET_TRANSFORM'):
             acc= multivariateShapeletMethod(X, y, 0.8)
         else: 
@@ -193,9 +222,5 @@ def main():
         file_write.close()
 
 
+
 main()
-
-
-
-
-
